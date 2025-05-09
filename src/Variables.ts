51181add292e72c2563dfa5bafcd688ac63d1b14
Eros <eros.montin@gmail.com>
2025-05-09 12:46:12 -0400

    variables

--- src/Variables.ts
+++ src/Variables.ts
@@ -9,8 +9,11 @@ export const JOBSAPI = `${CAMRIE_URL}/downloads`;
 
 export const DATAUPLODAAPI = `${CAMRIE_URL}/uploaddata`;
 
-export const ROI_UPLOAD= `${CAMRIE_URL}/uploadroi`;
+export const ROI_UPLOAD="https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/uploads";
 
-export const ROI_GET= `${CAMRIE_URL}/getroi`;
+
+export const ROI_GET= "https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/getrois";
+
+
 
 export const UNZIP= `${CAMRIE_URL}/unzip`;
